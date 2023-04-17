8e212e4d53050cf0338e510e45cabc1668410466
stBogdan95 <wm.stbogdan95@gmail.com>
2023-04-17 20:56:20 +0300

    test update svg network

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a4bb7c3b2791163809969e2bcac7f864",
-"/": "a4bb7c3b2791163809969e2bcac7f864",
-"main.dart.js": "e5cad69a349a04cf0a6dbf92d224ff50",
+"index.html": "fcc478a648daedaed05a1072eb046b2c",
+"/": "fcc478a648daedaed05a1072eb046b2c",
+"main.dart.js": "469fa8e897b58da7e7d07f2634a30818",
 "manifest.json": "c7e541d8a02a7eeed431df067b448d2e",
 "version.json": "9b818ca9511483c901bed1545384376c"
 };
